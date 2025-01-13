6d976f914d91e52ee6ac5266c089c258ed3d37f7
Pushpit jain <pj325284@gmail.com>
2025-01-13 19:52:34 +0000

    Jan 14, 2025, 1:22 AM

--- Deploy-Service/app.ts
+++ Deploy-Service/app.ts
@@ -1,7 +1,23 @@
 import dotenv from "dotenv";
+import { sqsPopper } from "./sqsPopper";
 
 dotenv.config();
 
+const hero1 = async () => {
+  let waitingTime = 5;
+  while (true) {
+    const x = await sqsPopper();
+    if (x) {
+      console.log(x);
+      waitingTime = 5;
+    } else {
+      console.log("No Message, waiting for ", waitingTime, " seconds\n");
+      await new Promise((resolve) => setTimeout(resolve, waitingTime * 1000));
+      if (waitingTime < 60) {
+        waitingTime += 1;
+      }
+    }
+  }
+};
 
-
-
+hero1();
---
+++ Deploy-Service/sqsPopper.ts
@@ -0,0 +1,45 @@
+import { SQS } from "@aws-sdk/client-sqs";
+import { config } from "dotenv";
+
+config();
+
+const sqsClient = new SQS({
+  region: process.env.AWS_REGION || "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "accessKeyId",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "secretAccessKey",
+  },
+});
+
+export const sqsPopper = async () => {
+  try {
+    // console.log("Getting from Queue");
+    const data = await sqsClient.receiveMessage({
+      QueueUrl: process.env.AWS_QUEUE_URL || "Default Queue URL",
+    });
+    // console.log(data);
+    if (data.Messages) {
+      // console.log("Success", data.Messages);
+      try {
+        await sqsClient.deleteMessage({
+          QueueUrl: process.env.AWS_QUEUE_URL || "Default Queue URL",
+          ReceiptHandle: data.Messages[0].ReceiptHandle,
+        });
+      } catch (error) {
+        console.log("Error in deleting message", error);
+        return error;
+      }
+      return data.Messages[0].Body;
+    }
+    // console.log("No Messages");
+    return null;
+  } catch (err) {
+    console.log("Error :\n", err);
+    return err;
+  }
+};
+
+if (require.main === module) {
+  const op = async ()=>{console.log(await sqsPopper());}
+  op();
+}
