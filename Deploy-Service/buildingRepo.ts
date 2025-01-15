71a88100d68bb0f7a13f59e297ff402ea7c3634f
Pushpit jain <pj325284@gmail.com>
2025-01-15 05:55:38 +0000

    Add logging for stdout and stderr in buildProject function and handle child process exit

--- Deploy-Service/buildingRepo.ts
+++ Deploy-Service/buildingRepo.ts
@@ -4,5 +4,19 @@ import path from "path";
 
 export async function buildProject(id: string) {
     const child = exec(`cd ${path.join(__dirname,"output",id)} && npm install && npm run build`)
-
+    child.stdout?.on("data", (data) => {
+        console.log( "stdout :", data);
+    });
+    child.stderr?.on("data", (data) => {
+        console.error("stderr :",data);
+    });
+    return new Promise((resolve, reject) => {
+        child.on("exit", (code) => {
+            if (code === 0) {
+                resolve("");
+            } else {
+                reject();
+            }
+        });
+    });
 }
