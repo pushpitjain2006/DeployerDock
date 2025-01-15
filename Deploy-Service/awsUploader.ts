36869dd849c054b407b2e2e028191265435f5e2f
Pushpit jain <pj325284@gmail.com>
2025-01-15 13:44:02 +0000

    Add console log for local file path in uploadFile function

--- Deploy-Service/awsUploader.ts
+++ Deploy-Service/awsUploader.ts
@@ -52,6 +52,7 @@ const s3Client = new S3Client({
 export const uploadFile = async (fileName: string, localFilePath: string) => {
   //fileName = output/12ae3../index.html
   //filePath = Users/username/vercel-clone/output/12ae3../index.html i.e. (__dirname + "/" + fileName)
+  console.log(localFilePath);
   getAllFiles(localFilePath).forEach(async (filePath) => {
     const fileContent = fs.readFileSync(filePath);
     const params = {
