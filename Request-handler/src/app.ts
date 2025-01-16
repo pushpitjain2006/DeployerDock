5c5abc70e1aad720b8e595b06170a0e898d9858c
Pushpit jain <pj325284@gmail.com>
2025-01-16 16:59:01 +0000

    Update server port configuration to use environment variable

---
+++ Request-handler/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/*",(req,res)=>{
+  const host = req.hostname;
+  const id = host.split(".")[0];
+  res.send(`Hello ${id} \n hostname: ${host}`);
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
--- Upload-Service/app.ts
+++ Upload-Service/app.ts
@@ -58,6 +58,7 @@ app.post("/deploy", async (req, res) => {
   res.json({ repoId, queueMessageId });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
