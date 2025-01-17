d60fd3512891dba2e8bbd5166cb6ca8e3dd1e743
Pushpit jain <pj325284@gmail.com>
2025-01-17 05:39:18 +0000

    AutoCommit: Add S3 client integration and dotenv configuration, update dependencies

--- Request-handler/src/index.ts
+++ Request-handler/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { SQS } from '@aws-sdk/client-sqs';
+import dotenv from "dotenv";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
+dotenv.config();
 const app = express();
 
-const sqsClient = new SQS({
+const S3client = new S3Client({
   region: process.env.AWS_REGION || "ap-south-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || "accessKeyId",
@@ -11,11 +13,22 @@ const sqsClient = new SQS({
   },
 });
 
-app.get("/*", (req, res) => {
+app.get("/*", async (req, res) => {
   const host = req.hostname;
   const id = host.split(".")[0];
   const filePath = req.path;
-  
+  const bucketName = process.env.BUCKET_NAME || "bucketName";
+  const file = await S3client.send(
+    new GetObjectCommand({
+      Bucket: bucketName,
+      Key: `dist/${id}${filePath}`,
+    })
+  );
+  const Type = filePath.endsWith("html")
+    ? "text/html"
+    : "application/javascript";
+  res.set("Content-Type", Type);
+  res.type(Type).send(file.Body);
 });
 
 const PORT = process.env.PORT || 3001;
