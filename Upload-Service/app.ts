25d94a5ad75cadf844b1f111d2677094a82cc6c2
Pushpit jain <pj325284@gmail.com>
2025-03-12 20:35:03 +0530

    Upgrade AWS SDK to 2.7.16

--- Upload-Service/app.ts
+++ Upload-Service/app.ts
@@ -11,7 +11,7 @@ import { sendToQueue } from "./sqsSender";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middlewares
 app.use(cors());
